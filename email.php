38875b6de78031400c51dca8b94bacf2a0bc0488
waltzacasio <WaltzAcasio@yahoo.com>
2024-01-06 17:41:23 +0800

    added email functionality, continue with changing submit button

--- email.php
+++ email.php
@@ -1,20 +1,29 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$json_data = file_get_contents("php://input");
+$data = json_decode($json_data, true);
+
+$email = $data['email'];
+$subject = $data['subject'];
+$message = "From: " . $email . "\n\n" . $data['message'];
 
 $to = "walter@walterdev.info";
-    $subject = "Record Keeping Web App Inquiry";
-    $messageWithEmailFrom = "From: " . $email . $message;
-    
-    // Send the email
-    $mailSent = mail($to, $subject, $messageWithEmailFrom);
-    
-    if (!$mailSent) {
-        error_log("Failed to send email.\n", 3, "failed_email.log");
+
+
+
+    $headers = 'From: WalterDev.Info'; // Replace with your actual email address
+
+    if (mail($to, $subject, $message, $headers)) {
+        $response = ['message' => 'Email sent successfully'];
+    } else {
+        $response = ['message' => 'Failed to send email, please try again later.'];
     }
 
-}
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}   else {
+        header('HTTP/1.1 405 Method Not Allowed');
+        echo 'Method Not Allowed';
+    }
+?>
